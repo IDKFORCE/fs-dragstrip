5f7f02f4cd85730cc1e2a8c060c53d61870ee6a5
GhostFace Scripts <71199348+IDKFORCE@users.noreply.github.com>
2024-11-09 01:28:50 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,5 +1,8 @@
-fx_version 'bodacious'
-games {'gta5'}
-author '⸸♱♥IDK_FORCE♥♱⸸#7611'
-description 'fs_ymaps'
+fx_version 'cerulean'
+game 'gta5'
+
+author 'GhostFaceScripts'
+description 'GhostFace DragStrip'
+version '1.0.0'
+
 this_is_a_map 'yes'
